ce99aaf9cfc3f8288ad866b3a2f78d2ca9e298a1
DeeptejD <deeptejdhauskar2003@gmail.com>
2025-01-18 22:21:43 +0530

    Added Location permissions.

---
+++ App/lib/services/providers/permissions.dart
@@ -0,0 +1,92 @@
+import 'dart:developer' as dev;
+
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Permissions extends ChangeNotifier with WidgetsBindingObserver {
+  Position? position;
+  bool locationAvailable = false;
+  bool waitingForLocationSettings = false;
+
+  // This is used to observe lifecycle changes for the app so when it returns from the settings screen we can detect it
+  Permissions() {
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed && waitingForLocationSettings) {
+      waitingForLocationSettings = false;
+      // once u return from the settings screen call fetch position again
+      fetchPosition();
+    }
+  }
+
+  Future<void> fetchPosition() async {
+    // checks if location is enabled
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      dev.log('Location Service not enabled.');
+      locationAvailable = false;
+      notifyListeners();
+
+      Fluttertoast.showToast(
+        msg: "Location service is disabled. Please enable it in settings.",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.CENTER,
+      );
+
+      waitingForLocationSettings = true;
+      // opens location settings
+      await Geolocator.openLocationSettings();
+
+      return;
+    }
+
+    checkAndRequestLocationPermission();
+  }
+
+  Future<void> checkAndRequestLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        dev.log('Location permission was denied.');
+
+        Fluttertoast.showToast(
+          msg:
+              "Location permission is denied. Please enable it in app settings.",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.CENTER,
+        );
+        // opens app settings
+        Geolocator.openAppSettings();
+      }
+    }
+
+    // fetches the current position
+    position = await Geolocator.getCurrentPosition();
+    locationAvailable = true;
+    notifyListeners();
+  }
+
+  void startListening() {
+    Geolocator.getPositionStream().listen(
+      (Position newPosition) {
+        position = newPosition;
+        locationAvailable = true;
+        notifyListeners();
+      },
+    );
+  }
+}
