265c8c4f4f9c3ccfeeaaf073b5a39b8f884fa841
joshuadlima <joshd11723@gmail.com>
2025-02-01 02:51:02 +0530

    herotag exception resolved

--- App/lib/pages/home_screen.dart
+++ App/lib/pages/home_screen.dart
@@ -39,6 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
         leading: Padding(
           padding: const EdgeInsets.all(10.0),
           child: FloatingActionButton(
+            heroTag: "hamburger",
             onPressed: () {
               _scaffoldKey.currentState?.openDrawer();
             },
