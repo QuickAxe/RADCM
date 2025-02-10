8bb703c2555ee01a7d5d2abf0a597947010f9293
DeeptejD <deeptejdhauskar2003@gmail.com>
2025-02-10 21:25:12 +0530

    fix: clicking markers now works as expected
    - i trippin'

--- admin_app/lib/services/providers/anomaly_marker_layer.dart
+++ admin_app/lib/services/providers/anomaly_marker_layer.dart
@@ -1,4 +1,3 @@
-import 'package:admin_app/services/anomaly_marker_service.dart';
 import 'package:admin_app/services/providers/user_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -6,50 +5,13 @@ import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:provider/provider.dart';
 
 import '../../data/models/anomaly_marker.dart';
+import '../../pages/anomaly_detail_page.dart';
+import '../../utils/marker_utils.dart';
+import '../anomaly_marker_service.dart';
 
 // This is the layer that displays the anomalies
 class AnomalyMarkerLayer extends StatelessWidget {
   const AnomalyMarkerLayer({super.key});
-
-  // this is for the marker clusters, color increases with more and more anomalies
-  Color _getClusterColor(int count) {
-    if (count < 5) {
-      return Colors.yellow.withOpacity(0.7); // Few anomalies
-    } else if (count < 10) {
-      return Colors.orange.withOpacity(0.7); // Moderate anomalies
-    } else {
-      return Colors.red.withOpacity(0.7); // High number of anomalies
-    }
-  }
-
-  // gets the appropriate icon (image asset path) TODO: Mapping redundant, also exists in settings create a single src
-  String getAnomalyIcon(String cat) {
-    String imageAssetPath = switch (cat) {
-      "Speedbreaker" => "assets/icons/ic_speedbreaker.png",
-      "Rumbler" => "assets/icons/ic_rumbler.png",
-      "Obstacle" => "assets/icons/ic_obstacle.png",
-      "Pothole" => "assets/icons/ic_pothole.png",
-      _ => "assets/icons/ic_obstacle.png",
-    };
-    return imageAssetPath;
-  }
-
-  void _showAnomalyDialog(BuildContext context, AnomalyMarker anomaly) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(anomaly.category),
-          content: Text(anomaly.location.toString()),
-          actions: [
-            TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Text("Ok")),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +29,14 @@ class AnomalyMarkerLayer extends StatelessWidget {
             rotate: true,
             point: anomaly.location,
             child: GestureDetector(
-              onTap: () => _showAnomalyDialog(context, anomaly),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AnomalyDetailPage(anomaly: anomaly),
+                  ),
+                );
+              },
               child: Image.asset(
                 getAnomalyIcon(anomaly.category),
                 width: 20.0,
@@ -83,7 +52,7 @@ class AnomalyMarkerLayer extends StatelessWidget {
             size: const Size(40, 40), // Cluster icon size
             markers: markers,
             builder: (context, markers) {
-              Color clusterColor = _getClusterColor(markers.length);
+              Color clusterColor = getClusterColor(markers.length);
 
               return Container(
                 decoration: BoxDecoration(
--- admin_app/lib/utils/marker_utils.dart
+++ admin_app/lib/utils/marker_utils.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 /// gets the appropriate icon (image asset path) TODO: Mapping redundant, also exists in settings create a single src
 String getAnomalyIcon(String cat) {
   String imageAssetPath = switch (cat) {
@@ -8,4 +10,15 @@ String getAnomalyIcon(String cat) {
     _ => "assets/icons/ic_obstacle.png",
   };
   return imageAssetPath;
+}
+
+/// this is for the marker clusters, color increases with more and more anomalies
+Color getClusterColor(int count) {
+  if (count < 5) {
+    return Colors.yellow.withOpacity(0.7); // Few anomalies
+  } else if (count < 10) {
+    return Colors.orange.withOpacity(0.7); // Moderate anomalies
+  } else {
+    return Colors.red.withOpacity(0.7); // High number of anomalies
+  }
 }
