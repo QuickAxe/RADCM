fef87eff4064d1ce48aa62efbf45062a380e9c1a
DeeptejD <deeptejdhauskar2003@gmail.com>
2025-02-10 21:09:03 +0530

    feat: added anomaly cluster

--- admin_app/lib/services/providers/anomaly_marker_layer.dart
+++ admin_app/lib/services/providers/anomaly_marker_layer.dart
@@ -1,116 +1,51 @@
-import 'package:admin_app/services/providers/permissions.dart';
+import 'package:admin_app/services/anomaly_marker_service.dart';
 import 'package:admin_app/services/providers/user_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:provider/provider.dart';
 
 import '../../data/models/anomaly_marker.dart';
-import '../../pages/anomaly_detail_page.dart';
-import '../../utils/marker_utils.dart';
-import '../anomaly_marker_service.dart';
 
 // This is the layer that displays the anomalies
 class AnomalyMarkerLayer extends StatelessWidget {
   const AnomalyMarkerLayer({super.key});
 
-  Future<void> _showAnomalyDialog(
-      BuildContext context, AnomalyMarker anomaly) async {
-    List<Placemark> placemarks = await placemarkFromCoordinates(
-        anomaly.location.latitude, anomaly.location.longitude);
-    print(placemarks.toString());
+  // this is for the marker clusters, color increases with more and more anomalies
+  Color _getClusterColor(int count) {
+    if (count < 5) {
+      return Colors.yellow.withOpacity(0.7); // Few anomalies
+    } else if (count < 10) {
+      return Colors.orange.withOpacity(0.7); // Moderate anomalies
+    } else {
+      return Colors.red.withOpacity(0.7); // High number of anomalies
+    }
+  }
 
+  // gets the appropriate icon (image asset path) TODO: Mapping redundant, also exists in settings create a single src
+  String getAnomalyIcon(String cat) {
+    String imageAssetPath = switch (cat) {
+      "Speedbreaker" => "assets/icons/ic_speedbreaker.png",
+      "Rumbler" => "assets/icons/ic_rumbler.png",
+      "Obstacle" => "assets/icons/ic_obstacle.png",
+      "Pothole" => "assets/icons/ic_pothole.png",
+      _ => "assets/icons/ic_obstacle.png",
+    };
+    return imageAssetPath;
+  }
+
+  void _showAnomalyDialog(BuildContext context, AnomalyMarker anomaly) {
     showDialog(
       context: context,
       builder: (context) {
-        bool checkboxOne = false; // Initial state
-        bool checkboxTwo = false;
-
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: Text(anomaly.category),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                      "${placemarks[0].street}, ${placemarks[0].locality}, ${placemarks[0].country} - ${placemarks[0].postalCode}"),
-                  Row(
-                    children: [
-                      Checkbox(
-                          value: checkboxOne,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              checkboxOne = value ?? false;
-                            });
-                          }),
-                      const Text("Confirm Inspection")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Checkbox(
-                          value: checkboxTwo,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              checkboxTwo = value ?? false;
-                            });
-                          }),
-                      const Text("Attach my signature")
-                    ],
-                  ),
-                ],
-              ),
-              actions: [
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: (checkboxOne && checkboxTwo)
-                          ? () {
-                              Position? pos = Provider.of<Permissions>(context,
-                                      listen: false)
-                                  .position;
-
-                              double distance = pos == null
-                                  ? 1000.0
-                                  : Geolocator.distanceBetween(
-                                      pos.latitude,
-                                      pos.longitude,
-                                      anomaly.location.latitude,
-                                      anomaly.location.longitude);
-
-                              if (distance >= 1000.0) {
-                                Fluttertoast.showToast(
-                                  msg:
-                                      "Anomaly is more than a kilometer away!!",
-                                  toastLength: Toast.LENGTH_LONG,
-                                );
-                              } else {
-                                Fluttertoast.showToast(
-                                  msg: "Anomaly Fixed!",
-                                  toastLength: Toast.LENGTH_LONG,
-                                );
-
-                                // remove this anomaly or whatever..
-
-                                Navigator.pop(context);
-                              }
-                            }
-                          : null,
-                      child: const Text("Mark fixed"),
-                    ),
-                    const SizedBox(width: 5),
-                    ElevatedButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("Cancel"),
-                    ),
-                  ],
-                ),
-              ],
-            );
-          },
+        return AlertDialog(
+          title: Text(anomaly.category),
+          content: Text(anomaly.location.toString()),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Ok")),
+          ],
         );
       },
     );
@@ -127,29 +62,46 @@ class AnomalyMarkerLayer extends StatelessWidget {
                 (anomaly) => userSettings.showOnMap[anomaly.category] ?? true)
             .toList();
 
-        return MarkerLayer(
-          // anomalies from the service class
-          markers: visibleAnomalies.map((anomaly) {
-            return Marker(
-                rotate: true, // so the icon stays upright
-                point: anomaly.location,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            AnomalyDetailPage(anomaly: anomaly),
-                      ),
-                    );
-                  },
-                  child: Image.asset(
-                    getAnomalyIcon(anomaly.category),
-                    width: 20.0,
-                    height: 20.0,
+        List<Marker> markers = visibleAnomalies.map((anomaly) {
+          return Marker(
+            rotate: true,
+            point: anomaly.location,
+            child: GestureDetector(
+              onTap: () => _showAnomalyDialog(context, anomaly),
+              child: Image.asset(
+                getAnomalyIcon(anomaly.category),
+                width: 20.0,
+                height: 20.0,
+              ),
+            ),
+          );
+        }).toList();
+
+        return MarkerClusterLayerWidget(
+          options: MarkerClusterLayerOptions(
+            maxClusterRadius: 45, // Adjust clustering sensitivity
+            size: const Size(40, 40), // Cluster icon size
+            markers: markers,
+            builder: (context, markers) {
+              Color clusterColor = _getClusterColor(markers.length);
+
+              return Container(
+                decoration: BoxDecoration(
+                  color: clusterColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Text(
+                    markers.length.toString(),
+                    style: const TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ));
-          }).toList(),
+                ),
+              );
+            },
+          ),
         );
       },
     );
