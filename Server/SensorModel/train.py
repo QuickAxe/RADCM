a3c434766d380acb5073960b881c8ca64047a8f5
quickaxe <manselismyname@gmail.com>
2025-02-16 21:54:21 +0530

    tiny changes

--- Server/SensorModel/train.py
+++ Server/SensorModel/train.py
@@ -23,20 +23,24 @@ EPOCHS = 100
 
 # the factor to smoothen the interpolated data by, duh!
 #! determine a good smoothing factor
-smoothingFactor = 50
+smoothingFactor = 0
 
 # the number of times to increase the data samples by, 
 # eg 200 data samples with a factor of 5 will output 1000 interpolated data samples 
 interpolationFactor = 5
 
 # number of cpu workers to assign to the dataloader (more is better, but make sure your cpu has enough threads)
-numWorkers = 3
+# ! Ok I'm not entirely sure how this works, so maybe leave it at 0
+numWorkers = 0
+
+# run number to save to it's corrosponding folder (yes yes, I know, but I'm too lazy to do this in code automatically)
+runNo = 2
 
 
 # ================================================== initialising =========================================================
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print("Device: ", device)
+print("Device: ", device)
 
 
 # ------------------------------------------- initilaise the dataloaders -------------------------------------------------
@@ -60,9 +64,18 @@ trainingDataloader = DataLoader(
     trainSet, batch_size=batchSize, shuffle=True, num_workers=numWorkers, pin_memory=True
 )
 
-# print("Training set has {} instances".format(len(trainSet)))
-# print("Validation set has {} instances".format(len(validationSet)))
+print("Training set has {} instances".format(len(trainSet)))
+print("Validation set has {} instances".format(len(validationSet)))
 
+# attempting to load all the data to device in one shot 
+# ! this did nothing :(
+# for data, target in validationDataloader:
+#     data = data.to('cuda')
+#     target = target.to('cuda')
+
+# for data, target in trainingDataloader:
+#     data = data.to('cuda')
+#     target = target.to('cuda')
 
 # =============================================== training loop part now ================================================
 
@@ -86,9 +99,10 @@ def train_one_epoch(epoch_index):
         # Every data instance is an input + label pair
 
         inputs, labels = data
-        
+
         inputs = inputs.to(device)
         labels = labels.to(device)
+
 
         # Zero your gradients for every batch!
         optimiser.zero_grad()
@@ -120,65 +134,75 @@ def train():
 
     best_vloss = 1_000_000.0
 
-    os.makedirs("./runs1", exist_ok=True)
-    with open("./runs1/results.csv", "a") as f:
+    os.makedirs(f"./runs{runNo}", exist_ok=True)
+
+    # with open("./runs1/results.csv", "a") as f:
+    #     writer = csv.writer(f)
+    # writer.writerow(["Epoch", "Train_Loss", "Val_Loss"])
+
+    writerList = [["Epoch", "Train_Loss", "Val_Loss"]]
+
+    for epoch in range(EPOCHS):
+        print("EPOCH {}:".format(epoch + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(epoch)
+
+        running_vloss = 0.0
+
+        # Set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        model.eval()
+
+        correct  = 0
+        # Disable gradient computation and reduce memory consumption.
+        with torch.no_grad():
+            for i, vdata in enumerate(validationDataloader):
+                vinputs, vlabels = vdata
+                
+                vinputs = vinputs.to(device)
+                vlabels = vlabels.to(device)
+
+                voutputs = model(vinputs)
+                vloss = lossFunction(voutputs, vlabels)
+                correct += (voutputs.argmax(1) == vlabels).type(torch.float).sum().item()
+                running_vloss += vloss
+
+        avg_vloss = running_vloss / len(validationDataloader)
+        running_vloss = 0.0
+        correct /= len(validationDataloader.dataset)
+
+        print(
+            "EPOCH {} Losses: train {} valid {} Accuracy: {}%".format(epoch + 1, avg_loss, avg_vloss, (100*correct)),
+            end="\n\n",
+        )
+
+        # writer.writerow([epoch + 1, avg_loss, avg_vloss.item()])
+        writerList.append([epoch + 1, avg_loss, avg_vloss.item()])
+
+        # Log the running loss averaged per batch
+        # for both training and validation
+        # writer.add_scalars(
+        #     "Training vs. Validation Loss",
+        #     {"Training": avg_loss, "Validation": avg_vloss},
+        #     epoch_number + 1,
+        # )
+        # writer.flush()
+
+        # Track best performance, and save the model's state
+        if epoch % 10 == 0:
+            model_path = "./runs{}/model_{}.pt".format(runNo, epoch + 1)
+            torch.save(model.state_dict(), model_path)
+            # f.flush()
+
+
+    with open(f"./runs{runNo}/results.csv", "a") as f:
         writer = csv.writer(f)
-        writer.writerow(["Epoch", "Train_Loss", "Val_Loss"])
+        writer.writerows(writerList)
+        f.flush()
 
-        for epoch in range(EPOCHS):
-            print("EPOCH {}:".format(epoch + 1))
-
-            # Make sure gradient tracking is on, and do a pass over the data
-            model.train(True)
-            avg_loss = train_one_epoch(epoch)
-
-            running_vloss = 0.0
-            # Set the model to evaluation mode, disabling dropout and using population
-            # statistics for batch normalization.
-            model.eval()
-
-            correct  = 0
-            # Disable gradient computation and reduce memory consumption.
-            with torch.no_grad():
-                for i, vdata in enumerate(validationDataloader):
-                    vinputs, vlabels = vdata
-                    vinputs = vinputs.to(device)
-                    vlabels = vlabels.to(device)
-
-                    voutputs = model(vinputs)
-                    vloss = lossFunction(voutputs, vlabels)
-                    correct += (voutputs.argmax(1) == vlabels).type(torch.float).sum().item()
-                    running_vloss += vloss
-
-            avg_vloss = running_vloss / len(validationDataloader)
-            running_vloss = 0.0
-            correct /= len(validationDataloader.dataset)
-
-            print(
-                "EPOCH {} Losses: train {} valid {} Accuracy: {}%".format(epoch + 1, avg_loss, avg_vloss, (100*correct)),
-                end="\n\n",
-            )
-
-            
-            writer.writerow([epoch + 1, avg_loss, avg_vloss.item()])
-
-            # Log the running loss averaged per batch
-            # for both training and validation
-            # writer.add_scalars(
-            #     "Training vs. Validation Loss",
-            #     {"Training": avg_loss, "Validation": avg_vloss},
-            #     epoch_number + 1,
-            # )
-            # writer.flush()
-
-            # Track best performance, and save the model's state
-
-            if epoch % 10 == 0:
-                model_path = "./runs1/model_{}.pt".format(epoch + 1)
-                torch.save(model.state_dict(), model_path)
-                f.flush()
-
-    filePath = "./runs1/results.csv"
+    filePath = f"./runs{runNo}/results.csv"
     df = pd.read_csv(filePath)
 
     x = df.iloc[:, 0]
@@ -194,7 +218,7 @@ def train():
     plt.legend()
     plt.grid(True)
 
-    plt.savefig("./runs1/results.png", dpi=300, bbox_inches="tight")
+    plt.savefig(f"./runs{runNo}/results.png", dpi=300, bbox_inches="tight")
     plt.close()
 
 
