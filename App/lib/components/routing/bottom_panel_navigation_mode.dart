96f46c2cbf133752e5d0ceb0523a3600bf42ca95
DeeptejD <deeptejdhauskar2003@gmail.com>
2025-02-22 18:23:08 +0530

    Refactor

--- App/lib/components/routing/bottom_panel_navigation_mode.dart
+++ App/lib/components/routing/bottom_panel_navigation_mode.dart
@@ -1,3 +1,4 @@
+import 'package:app/components/OSM_Attribution.dart';
 import 'package:app/components/routing/dynamic_route_directions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -6,7 +7,6 @@ import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../services/providers/anomaly_marker_layer.dart';
 import '../../services/providers/route_provider.dart';
@@ -32,7 +32,7 @@ class NavigationMode extends StatelessWidget {
       color: Theme.of(context).colorScheme.surfaceContainer,
       borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(25.0), topRight: Radius.circular(25.0)),
-      minHeight: 200,
+      minHeight: 250,
       maxHeight: 200,
       panel: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -103,18 +103,10 @@ class NavigationMode extends StatelessWidget {
               markerSize: const Size(40, 40),
             ),
           ),
-          Positioned(
+          const Positioned(
             left: 200,
-            bottom: 200,
-            child: RichAttributionWidget(
-              attributions: [
-                TextSourceAttribution(
-                  'OpenStreetMap contributors',
-                  onTap: () => launchUrl(
-                      Uri.parse('https://openstreetmap.org/copyright')),
-                ),
-              ],
-            ),
+            bottom: 250,
+            child: Attribution(),
           ),
         ],
       ),
