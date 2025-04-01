ecd721f2ada8e4853f938fa589d4d54243f5fe35
DeeptejD <deeptejdhauskar2003@gmail.com>
2025-04-01 13:03:36 +0530

    Improved the search UX, snackBars are shown if search runs into errors with appropriate error messages

--- App/lib/services/providers/search.dart
+++ App/lib/services/providers/search.dart
@@ -1,31 +1,45 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Search extends ChangeNotifier {
   List<dynamic> searchSuggestions = [];
   bool isCurrentSelected = false;
   bool loadingResults = false;
   dynamic currentSelected;
+  String? errorMessage;
 
   // Fetch multiple places for a search query
   Future<void> getSuggestions(String query) async {
     loadingResults = true;
+    errorMessage = null;
     notifyListeners();
 
-    final url = Uri.parse('https://nominatim.openstreetmap.org/search?q=$query&format=json&addressdetails=1&limit=50');
-    final response = await http.get(url);
+    searchSuggestions.clear(); // clear previous search results
 
-    if (response.statusCode == 200) {
-      searchSuggestions = json.decode(response.body);
+    try {
+      final url = Uri.parse(
+          'https://nominatim.openstreetmap.org/search?q=$query&format=json&addressdetails=1&limit=50');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        searchSuggestions = json.decode(response.body);
+      } else {
+        errorMessage = "Unexpected error: ${response.statusCode}";
+      }
+    } on SocketException {
+      errorMessage = "No internet connection. Check your network.";
+    } on TimeoutException {
+      errorMessage = "Server timeout. Try again later.";
+    } catch (e) {
+      errorMessage = "An unexpected error occurred.";
+    } finally {
       loadingResults = false;
       notifyListeners();
-    } else {
-      loadingResults = false;
-      notifyListeners();
-      throw Exception('Failed to fetch search results');
     }
-
   }
 
   void performSelection(dynamic place) {
