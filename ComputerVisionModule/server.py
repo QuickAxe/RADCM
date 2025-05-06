81ae60dbe7694e365141b7d06288cc5b764d289a
quickaxe <manselismyname@gmail.com>
2025-05-06 17:43:06 +0530

    fixed bug where messages wouldn't send properly

--- ComputerVisionModule/server.py
+++ ComputerVisionModule/server.py
@@ -17,60 +17,72 @@ class Server(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        self.send_response(200)
+        
         self.send_header('Content-type', 'text/html')
-        
-        
+        self.end_headers()  
+
         data = json.loads(post_data)
+
+        message = ""
 
         if data["command"] == "start":
             # add a message to the global queue to start a new survey 
             
             if (self.messageQue.empty()):
                 # if there's no previous "start survey" message in the queue
-                self.send_response(200, "got command successfully")
-                self.messageQue.put("start")
                 
+                self.send_response(200)
+                self.messageQue.put("start")
+
+                message =  "got command successfully"         
             
             elif( not self.messageQue.empty() and self.messageQue.queue[0] == "stop"):
-                self.send_response(400, message="Start command given, but previous survey is still being stopped, please wait...")
+                self.send_response(400)
+                message="Start command given, but previous survey is still being stopped, please wait..."
 
             else:
-                self.send_response(400, message="Start command given, but survey already going on, dummy")
+                self.send_response(400)
+                message="Start command given, but survey already going on, dummy"
         
         elif data["command"] == "stop":
             # stop survey, if it's going on, otherwise return an error message and stuff
             
             if (self.messageQue.empty()):
                 # what on earth, trying to stop a survey that isn't even happening !!
-                self.send_response(400, message="Stop command given, but no survey currently going on, dummy")
+                self.send_response(400)
+                message="Stop command given, but no survey currently going on, dummy"
             
             elif( not self.messageQue.empty() and self.messageQue.queue[0] == "stop"):
-                self.send_response(400, message="Stop command given, but previous survey is still being stopped, please wait...")
+                self.send_response(400)
+                message="Stop command given, but previous survey is still being stopped, please wait..."
             
             else:
                 # valid stop command, do the needful 
                 # remove start message from queue
-                self.send_response(200, "got command successfully")
+                self.send_response(200)
+                message = "got command successfully"
+
                 self.messageQue.get()
                 self.messageQue.put("stop")
                 
        
         elif data["command"] == "sendImages":
             if( not self.messageQue.empty() and self.messageQue.queue[0] == "start"):
-                self.send_response(400, message="SendImages command given, but previous survey is still going on, please end survey first...")
+                self.send_response(400)
+                message="SendImages command given, but previous survey is still going on, please end survey first..."
             else:
                 self.messageQue.put("sendImages")
-                self.send_response(200, "got command successfully")
+                self.send_response(200)
+                message = "got command successfully"
 
 
         else:
-            self.send_response(400, message="wrong command sent, what are you on friendo? (send me some too, instead of whatever message you sent)")
+            self.send_response(400)
+            message="wrong command sent, what are you on friendo? (send me some too, instead of whatever message you sent)"
         
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()        
-
-
+        
+        #  Write the message to the response body
+        self.wfile.write(message.encode())  
 
 def runServer( messageQue, port=3333):
     
